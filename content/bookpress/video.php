66a2a4830d1643e5fb6912b05c4d1aab1e332356
Olivier Baumann <olivier@ovbm.co>
2017-05-30 14:56:26 +0200

    fixed concert link

--- content/bookpress/video.php
+++ content/bookpress/video.php
@@ -2,6 +2,6 @@
 			<h3>IKARUS live at Theater am Gleis, complete first set, March 1st 2017</h3>
 			<iframe width="640" height="360" src="https://www.youtube.com/embed/videoseries?list=PLZk08irTcdtRFGs_BXesdsIglyani9uFE" frameborder="0" allowfullscreen></iframe>
 			<h3>IKARUS Japan Tour 2017</h3>
-			<iframe class="ytvideo" width="480" height="270" data-src="https://www.youtube.com/embed/KRp5ieqnz0E?rel=0&amp;controls=0&amp;showinfo=1" src="" frameborder="0" allowfullscreen></iframe>
+			<iframe width="640" height="360" src="https://www.youtube.com/embed/KRp5ieqnz0E?rel=0&amp;controls=0&amp;showinfo=1" frameborder="0" allowfullscreen></iframe>
 		</div>
 	<div style="clear:both"></div>
