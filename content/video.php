4a8fc75fc51bd77f697de6f38a40740304b8784b
Olivier Baumann <olivier@ovbm.co>
2018-01-25 20:08:46 +0100

    ikarus caliph video added

--- content/video.php
+++ content/video.php
@@ -1,5 +1,7 @@
+<iframe width="480" height="270" src="https://www.youtube.com/embed/YXkpAqI1HAw?rel=0&amp;controls=0&amp;showinfo=1" frameborder="0" allowfullscreen></iframe>
+
 <iframe width="480" height="270" src="https://www.youtube.com/embed/videoseries?list=PLZk08irTcdtRFGs_BXesdsIglyani9uFE" frameborder="0" allowfullscreen></iframe>
 
-<iframe class="ytvideo" width="480" height="270" data-src="https://www.youtube.com/embed/KRp5ieqnz0E?rel=0&amp;controls=0&amp;showinfo=1" src="" frameborder="0" allowfullscreen></iframe>
+<!-- <iframe class="ytvideo" width="480" height="270" data-src="https://www.youtube.com/embed/KRp5ieqnz0E?rel=0&amp;controls=0&amp;showinfo=1" src="" frameborder="0" allowfullscreen></iframe> -->
 
 <p><a class='track' data-category='link' data-action='youtube' target="_blank" class="ytlink" href="https://www.youtube.com/channel/UCF5VD2G4hRGEJuScWgYWN6A">ikarus youtube</a></p>
